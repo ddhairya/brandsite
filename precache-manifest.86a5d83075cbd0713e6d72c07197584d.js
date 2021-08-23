36b5b34bfb072ac5f9c156cdf84d650552e44027
ddhairya <d_dhairya@yahoo.com>
2021-08-23 11:13:01 +0400

    Updates

---
+++ precache-manifest.86a5d83075cbd0713e6d72c07197584d.js
@@ -0,0 +1,206 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e322bdc5fa227baeec566779aa3a75d",
+    "url": "/brandsite/index.html"
+  },
+  {
+    "revision": "c79ace81cfca741bb126",
+    "url": "/brandsite/static/css/14.e94d5e4c.chunk.css"
+  },
+  {
+    "revision": "b2e2dfc43214bbe65496",
+    "url": "/brandsite/static/css/main.479d4fab.chunk.css"
+  },
+  {
+    "revision": "c79ace81cfca741bb126",
+    "url": "/brandsite/static/js/14.ea9742d0.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "/brandsite/static/js/14.ea9742d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2e2dfc43214bbe65496",
+    "url": "/brandsite/static/js/main.9cbc45b7.chunk.js"
+  },
+  {
+    "revision": "4b5e46beb4bcd3f61548",
+    "url": "/brandsite/static/js/reactPlayerDailyMotion.72aafc81.chunk.js"
+  },
+  {
+    "revision": "0f332de8108fbf5bbc46",
+    "url": "/brandsite/static/js/reactPlayerFacebook.a4a075f2.chunk.js"
+  },
+  {
+    "revision": "072769bebe7304fdf05e",
+    "url": "/brandsite/static/js/reactPlayerFilePlayer.286baa9b.chunk.js"
+  },
+  {
+    "revision": "e8a408bfbdabc50afe98",
+    "url": "/brandsite/static/js/reactPlayerMixcloud.b6c4af2b.chunk.js"
+  },
+  {
+    "revision": "73614a58ca95f7d3b94f",
+    "url": "/brandsite/static/js/reactPlayerPreview.21d05b81.chunk.js"
+  },
+  {
+    "revision": "cfdb6b908ae54aa6d08a",
+    "url": "/brandsite/static/js/reactPlayerSoundCloud.817495f4.chunk.js"
+  },
+  {
+    "revision": "9e61e4d015298ae19159",
+    "url": "/brandsite/static/js/reactPlayerStreamable.b46f39d0.chunk.js"
+  },
+  {
+    "revision": "69b87abf4db39462e7e0",
+    "url": "/brandsite/static/js/reactPlayerTwitch.141ffdd6.chunk.js"
+  },
+  {
+    "revision": "c4f9f1253ccd18546873",
+    "url": "/brandsite/static/js/reactPlayerVidyard.4670e39b.chunk.js"
+  },
+  {
+    "revision": "de63dfac0cd3e1d03b58",
+    "url": "/brandsite/static/js/reactPlayerVimeo.2fe3215a.chunk.js"
+  },
+  {
+    "revision": "9b52f8619b2180a4979e",
+    "url": "/brandsite/static/js/reactPlayerWistia.2d3d794c.chunk.js"
+  },
+  {
+    "revision": "4074a7d3bf4503d5180a",
+    "url": "/brandsite/static/js/reactPlayerYouTube.cf5c0722.chunk.js"
+  },
+  {
+    "revision": "540ced970e912ae8c88e",
+    "url": "/brandsite/static/js/runtime-main.0783e103.js"
+  },
+  {
+    "revision": "624af760fa085add3920a38be25ce4b0",
+    "url": "/brandsite/static/media/01.624af760.jpg"
+  },
+  {
+    "revision": "928c8071c2d0e7ae1f120b0f8618e0a0",
+    "url": "/brandsite/static/media/01.928c8071.mp4"
+  },
+  {
+    "revision": "9fd1a3d57e971bbb228d1779cab43c3c",
+    "url": "/brandsite/static/media/02.9fd1a3d5.jpg"
+  },
+  {
+    "revision": "ef42ab726cf05cf1aabcd6d57c430fa1",
+    "url": "/brandsite/static/media/03.ef42ab72.jpg"
+  },
+  {
+    "revision": "fd30f7185b2ade3f6a4cce7badcf5b9c",
+    "url": "/brandsite/static/media/04.fd30f718.jpg"
+  },
+  {
+    "revision": "37f773fa4411e27ddca142ad22be6bdf",
+    "url": "/brandsite/static/media/05.37f773fa.jpg"
+  },
+  {
+    "revision": "e993142be92679a9bc544f30dc5b712b",
+    "url": "/brandsite/static/media/06.e993142b.jpg"
+  },
+  {
+    "revision": "29d92ba62cb49904f75697883dba9263",
+    "url": "/brandsite/static/media/07.29d92ba6.jpg"
+  },
+  {
+    "revision": "75234d0378499da9fd79420a015f75e6",
+    "url": "/brandsite/static/media/08.75234d03.jpg"
+  },
+  {
+    "revision": "16dcda9367d3becfd4c270b72ef1b3ef",
+    "url": "/brandsite/static/media/09.16dcda93.jpg"
+  },
+  {
+    "revision": "071181569f4a917ec855f849ef0e310f",
+    "url": "/brandsite/static/media/10.07118156.jpg"
+  },
+  {
+    "revision": "c59a59fc8352d3d50d744918d329e355",
+    "url": "/brandsite/static/media/11.c59a59fc.jpg"
+  },
+  {
+    "revision": "a3e685e8552927e8117650aa52ec92ba",
+    "url": "/brandsite/static/media/12.a3e685e8.jpg"
+  },
+  {
+    "revision": "a6104e7d7a5b02a149ef9935ef88f619",
+    "url": "/brandsite/static/media/Adam.a6104e7d.woff2"
+  },
+  {
+    "revision": "d1a7af72bb95c116409e9dbbcb83edaf",
+    "url": "/brandsite/static/media/Adam.d1a7af72.woff"
+  },
+  {
+    "revision": "6f77cc3cd5161de4f2e4e4035ab19ffd",
+    "url": "/brandsite/static/media/Omnes.6f77cc3c.woff2"
+  },
+  {
+    "revision": "d37a2954e899145a272c70b26b2834b6",
+    "url": "/brandsite/static/media/Omnes.d37a2954.woff"
+  },
+  {
+    "revision": "ed0c8647cc96584e82f574d3eec36f1a",
+    "url": "/brandsite/static/media/Oswald.ed0c8647.woff2"
+  },
+  {
+    "revision": "d230917d4be0ec634caa4541207f8614",
+    "url": "/brandsite/static/media/b1.d230917d.jpg"
+  },
+  {
+    "revision": "ee37307afdfa384aa5f1cb9788477b42",
+    "url": "/brandsite/static/media/b3.ee37307a.jpg"
+  },
+  {
+    "revision": "95dcce719e39fbda162c6fa9f45ff3b8",
+    "url": "/brandsite/static/media/cw1.95dcce71.png"
+  },
+  {
+    "revision": "ea27b524b84289265d09842c25a00350",
+    "url": "/brandsite/static/media/cw2.ea27b524.png"
+  },
+  {
+    "revision": "b1fabd53bb817553ec995f7ce88ea75b",
+    "url": "/brandsite/static/media/cw3.b1fabd53.png"
+  },
+  {
+    "revision": "806dcef34867ecc8a39e0e318ef1e522",
+    "url": "/brandsite/static/media/lm1.806dcef3.png"
+  },
+  {
+    "revision": "41f039241415a329d6dc6f9b317395e7",
+    "url": "/brandsite/static/media/lm2.41f03924.png"
+  },
+  {
+    "revision": "a97c61e0c462c979377dcb4cf1cc39af",
+    "url": "/brandsite/static/media/lm3.a97c61e0.png"
+  },
+  {
+    "revision": "943729c29ea3a01d744bc96e6881eace",
+    "url": "/brandsite/static/media/mm1.943729c2.png"
+  },
+  {
+    "revision": "267e96b3027fbfbc4b73d30ea629f32b",
+    "url": "/brandsite/static/media/mm2.267e96b3.png"
+  },
+  {
+    "revision": "8056dec3a4692d637f9582ee0072e406",
+    "url": "/brandsite/static/media/mm3.8056dec3.png"
+  },
+  {
+    "revision": "9315c1ddbd4a732be024a4add5a7cc2b",
+    "url": "/brandsite/static/media/tp1.9315c1dd.png"
+  },
+  {
+    "revision": "bec7ed76d11840e3012b2c7e6f406bb9",
+    "url": "/brandsite/static/media/tp2.bec7ed76.png"
+  },
+  {
+    "revision": "f508c7f3abe6f289f1640572d6b5aea1",
+    "url": "/brandsite/static/media/tp3.f508c7f3.png"
+  }
+]);
